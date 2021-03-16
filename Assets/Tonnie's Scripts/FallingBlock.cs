2b0a04a478499eefa7547280bb11c54d04dc05f6
Sparkeagle2000 <43283207+Sparkeagle2000@users.noreply.github.com>
2021-03-15 20:43:40 -0400

    updated falling blocks script for timestop

--- Assets/Tonnie's Scripts/FallingBlock.cs
+++ Assets/Tonnie's Scripts/FallingBlock.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class FallingBlock : MonoBehaviour
 {
     Vector3 p;
-    Quaternion q;
+    Quaternion q,stopQ;
+    public Rigidbody rb;
+    public bool stop;
+    public bool first=true;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +24,24 @@ public class FallingBlock : MonoBehaviour
             transform.rotation=q;
             transform.position=p;
         }
+        if(stop&&first)
+        {
+            stopQ=transform.rotation;
+            Vector3 forceToAdd =new Vector3(0,0,0);
+ 
+            rb.velocity = forceToAdd;
+            rb.useGravity=false;
+            first=false;
+        }
+        if(stop)
+        {
+            transform.rotation=stopQ;
+        }
+        else
+        {
+            first=true;
+            rb.useGravity=true;
+        }
+        
     }
 }
