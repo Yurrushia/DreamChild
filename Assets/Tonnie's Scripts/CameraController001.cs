c3f1ff577fdbf9da6b47e91f03ed2d4ffa7ba89b
Sparkeagle2000 <43283207+Sparkeagle2000@users.noreply.github.com>
2021-03-15 20:07:39 -0400

    this be basic mechanics

---
+++ Assets/Tonnie's Scripts/CameraController001.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController001 : MonoBehaviour
+{
+    public float RotationSpeed=1;
+    public Transform target, player;
+    float x,y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible=false;
+        Cursor.lockState=CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        x+=Input.GetAxis("Mouse X")*RotationSpeed;
+        y-=Input.GetAxis("Mouse Y")*RotationSpeed;
+        y=Mathf.Clamp(y,-35,60);
+
+        transform.LookAt(target);
+        target.rotation=Quaternion.Euler(y,x,0);
+        player.rotation=Quaternion.Euler(0,x,0);
+    }
+}
---
+++ Assets/Tonnie's Scripts/PlayerController001.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController001 : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public KeyCode fire;
+    public KeyCode jump;
+
+    private float nextFire;
+    public Rigidbody rb;
+    public bool onGround=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+            transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
+            if(Input.GetKeyDown(jump)&&onGround)
+            {
+                Debug.Log("jumping");
+                rb.AddForce(new Vector3(0,7,0), ForceMode.Impulse);
+                onGround=false;
+            }
+                //Attack script for attack
+            //if (Input.GetKeyDown(fire) && Time.time > nextFire)
+            //{
+            //    nextFire=Time.time+fireRate;
+            //    Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+            //}
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag=="Floor")
+        {
+            onGround=true;
+        }
+    }
+}
