bef4f2a73b6d6548eb53f46efce78655705fef98
Sparkeagle2000 <43283207+Sparkeagle2000@users.noreply.github.com>
2021-03-15 20:22:47 -0400

    falling object script

---
+++ Assets/Tonnie's Scripts/FallingBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    Vector3 p;
+    Quaternion q;
+    // Start is called before the first frame update
+    void Start()
+    {
+        q=transform.rotation;
+        p=transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<-5)
+        {
+            transform.rotation=q;
+            transform.position=p;
+        }
+    }
+}
